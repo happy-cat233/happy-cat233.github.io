f6e068f46f19998ba930ba6e4604beebcf7c9441
Eric Co Opin <142472379+happy-cat233@users.noreply.github.com>
2025-07-23 17:25:11 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,1 @@
-window.alert("i'm not a robot?");//验证不是机器人
 
-function gift() {
-    document.getElementById("gift").innerHTML = "Have a nice day!"
-};//定义gift函数
-
-function HideImage() {
-    document.getElementById('nyan-cat').style.display = 'none';
-    document.getElementById('show').style.display = 'block';
-    document.getElementById('hide').style.display = 'none'
-}//隐藏函数
-function ShowImage() {
-    document.getElementById('nyan-cat').style.display = 'block';
-    document.getElementById('hide').style.display = 'block';
-    document.getElementById('show').style.display = 'none'
-}
